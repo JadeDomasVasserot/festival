6a899e4f8d2278c9ff95581c69bf036c9e32b184
Jade DOMAS-VASSEROT <75765424+JadeDomasVasserot@users.noreply.github.com>
2021-10-06 22:56:48 +0200

    Revert "Revert "Commit""
    This reverts commit bd220e6d28bae0aa799ff0ede4800c360aa8012c.

--- src/Form/ChanteurType.php
+++ src/Form/ChanteurType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 
 class ChanteurType extends AbstractType
