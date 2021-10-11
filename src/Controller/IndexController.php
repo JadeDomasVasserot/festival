f425310312580ac1bece32bb3e0acf1c38bbd712
Erine-Berard <berard.erine@gmail.com>
2021-10-11 14:46:58 +0200

    Ajout a vérifier

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Festival;
 
 class IndexController extends AbstractController
 {
@@ -23,4 +24,17 @@ class IndexController extends AbstractController
     {
         return $this->render('inscription.html.twig') ;
     }
+
+    /**
+     * @Route("/inscriptionRapide", name="inscriptionRapide")
+     */
+    public function inscriptionRapide(): Response
+    {
+        $festivals = $this->getDoctrine()
+            ->getRepository(Festival::class)
+            ->findAll();
+        return $this->render('inscriptionRapide.html.twig',[ 'festivals' => $festivals]);
+    }
+
+
 }
